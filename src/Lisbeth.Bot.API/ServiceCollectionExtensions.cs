601686e5276b8f19b56d6c30e863ee94e793a272
Krzysztof Kupisz <kupisz.krzysztof@gmail.com>
2021-12-24 00:58:55 +0100

    fix: pagination ack

--- src/Lisbeth.Bot.API/ServiceCollectionExtensions.cs
+++ src/Lisbeth.Bot.API/ServiceCollectionExtensions.cs
@@ -84,7 +84,9 @@ public static class ServiceCollectionExtensions
         {
             options.PaginationBehaviour = PaginationBehaviour.WrapAround;
             options.ResponseBehavior = InteractionResponseBehavior.Ack;
+            options.AckPaginationButtons = true;
             options.Timeout = TimeSpan.FromMinutes(2);
+            options.ResponseMessage = "You can't control paginated responses that are not meant for you!";
         });
 
         #endregion
