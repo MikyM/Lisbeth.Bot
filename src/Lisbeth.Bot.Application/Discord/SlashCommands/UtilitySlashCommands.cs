709c09a87975cd31f62f80f536784868edb3b6ff
Krzysztof Kupisz <kupisz.krzysztof@gmail.com>
2022-05-18 18:02:25 +0200

    feat: avatar util cmd

---
+++ src/Lisbeth.Bot.Application/Discord/SlashCommands/UtilitySlashCommands.cs
@@ -0,0 +1,47 @@
+﻿using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.SlashCommands;
+using Lisbeth.Bot.Application.Discord.Exceptions;
+using Lisbeth.Bot.Application.Discord.SlashCommands.Base;
+using Lisbeth.Bot.DataAccessLayer.Specifications.Guild;
+
+namespace Lisbeth.Bot.Application.Discord.SlashCommands;
+
+[SlashModuleLifespan(SlashModuleLifespan.Scoped)]
+[UsedImplicitly]
+public class UtilitySlashCommands : ExtendedApplicationCommandModule
+{
+    private readonly IGuildDataService _guildDataService;
+
+    public UtilitySlashCommands(IGuildDataService guildDataService)
+    {
+        _guildDataService = guildDataService;
+    }
+
+    [UsedImplicitly]
+    [SlashCommand("avatar", "Command that allows retrieving given user avatar, or your own.")]
+    public async Task BanCommand(InteractionContext ctx,
+        [Option("user", "User to retrieve avatar for if not for yourself")]
+        DiscordUser? user = null)
+    {
+        await ctx.DeferAsync();
+
+        var guildRes = await _guildDataService.GetSingleBySpecAsync(new ActiveGuildByIdSpec(ctx.Guild.Id));
+        if (!guildRes.IsDefined(out var guild))
+            throw new DiscordNotFoundException();
+        
+        user ??= ctx.Member;
+
+        if (user is not DiscordMember member)
+            throw new InvalidOperationException("Couldn't retrieve discord member");
+
+        var embed = new DiscordEmbedBuilder();
+        embed.WithColor(new DiscordColor(guild.EmbedHexColor));
+        embed.WithImageUrl(member.GetGuildAvatarUrl(ImageFormat.Auto));
+        embed.AddField("Default avatar URL", member.GetAvatarUrl(ImageFormat.Auto));
+        embed.AddField("Guild avatar URL", member.GetGuildAvatarUrl(ImageFormat.Auto));
+
+        await ctx.EditResponseAsync(
+            new DiscordWebhookBuilder().AddEmbed(embed));
+    }
+}
---
+++ src/Lisbeth.Bot.DataAccessLayer/Migrations/20220515170654_ReminderAdditionalInfo.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lisbeth.Bot.DataAccessLayer.Migrations
+{
+    public partial class ReminderAdditionalInfo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "reminder",
+                type: "varchar(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "hangfire_id",
+                table: "reminder",
+                type: "varchar(300)",
+                maxLength: 300,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(300)",
+                oldMaxLength: 300,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "should_add_creation_info",
+                table: "reminder",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "should_add_creation_info",
+                table: "reminder");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "reminder",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(200)",
+                oldMaxLength: 200);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "hangfire_id",
+                table: "reminder",
+                type: "varchar(300)",
+                maxLength: 300,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(300)",
+                oldMaxLength: 300);
+        }
+    }
+}
