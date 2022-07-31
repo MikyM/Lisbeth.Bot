661d4a1f7b4517b891a97eba0591fdaaee069e40
Krzysztof Kupisz <kupisz.krzysztof@gmail.com>
2022-07-31 18:54:22 +0200

    fix:...

---
+++ src/Lisbeth.Bot.DataAccessLayer/Migrations/20220731165328_member_fields.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Lisbeth.Bot.DataAccessLayer.Migrations
+{
+    public partial class member_fields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "punishment",
+                table: "member_history_entry",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "punishment_by_id",
+                table: "member_history_entry",
+                type: "bigint",
+                nullable: true)
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AddColumn<string>(
+                name: "punishment_by_username",
+                table: "member_history_entry",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "punishment_reason",
+                table: "member_history_entry",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "punishment",
+                table: "member_history_entry");
+
+            migrationBuilder.DropColumn(
+                name: "punishment_by_id",
+                table: "member_history_entry");
+
+            migrationBuilder.DropColumn(
+                name: "punishment_by_username",
+                table: "member_history_entry");
+
+            migrationBuilder.DropColumn(
+                name: "punishment_reason",
+                table: "member_history_entry");
+        }
+    }
+}
