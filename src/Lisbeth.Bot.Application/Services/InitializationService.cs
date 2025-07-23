f8389213656bb2fbe4739d212daaa949c728833f
Krzysztof Kupisz <kupisz.krzysztof@gmail.com>
2025-07-24 00:22:32 +0200

    revert: leave non default guilds

--- src/Lisbeth.Bot.Application/Services/InitializationService.cs
+++ src/Lisbeth.Bot.Application/Services/InitializationService.cs
@@ -1,5 +1,6 @@
 ﻿using Lisbeth.Bot.Domain;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace Lisbeth.Bot.Application.Services;
@@ -9,15 +10,21 @@ public class InitializationService : IHostedService
 {
     private readonly IDiscordService _discordService;
     private readonly IOptions<BotConfiguration> _options;
+    private readonly ILogger<InitializationService> _logger;
 
-    public InitializationService(IDiscordService discordService, IOptions<BotConfiguration> options)
+    public InitializationService(IDiscordService discordService, IOptions<BotConfiguration> options, ILogger<InitializationService> logger)
     {
         _discordService = discordService;
         _options = options;
+        _logger = logger;
     }
     
     public Task StartAsync(CancellationToken cancellationToken)
     {
+        _logger.LogInformation("Currently serving {GuildCount} guilds: {@Guilds}",
+            _discordService.Client.Guilds.Count,
+            _discordService.Client.Guilds.Select(x => new { x.Value.Id, x.Value.Name }));
+        
         return Task.CompletedTask;
         // CLEARS COMMANDS
         /*await _discordService.Client.BulkOverwriteGlobalApplicationCommandsAsync(Array.Empty<DiscordApplicationCommand>());
